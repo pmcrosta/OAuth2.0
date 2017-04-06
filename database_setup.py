76500da2e894cf590216ce552baed8e158ef634c
Peter Crosta <pmcrosta@gmail.com>
2017-04-06 19:39:49 -0400

    through google

--- database_setup.py
+++ database_setup.py
@@ -5,11 +5,22 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
+class User(Base):
+    __tablename__='user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    email = Column(String(80), nullable=False)
+    picture = Column(String(200))
+
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -17,6 +28,7 @@ class Restaurant(Base):
        return {
            'name'         : self.name,
            'id'           : self.id,
+           'user_id'      : self.user_id
        }
  
 class MenuItem(Base):
@@ -30,6 +42,8 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
     @property
@@ -41,11 +55,12 @@ class MenuItem(Base):
            'id'         : self.id,
            'price'         : self.price,
            'course'         : self.course,
+           'user_id'      : self.user_id
        }
 
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
  
 
 Base.metadata.create_all(engine)
